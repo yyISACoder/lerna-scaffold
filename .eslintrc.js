d352e74f38a695ee0373ac6e95761b5a8218b4ac
Mr.Yin <664960698@qq.com>
2023-03-22 19:10:15 +0800

    edit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: [
     // "eslint:recommended",
@@ -11,12 +11,16 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  plugins: ['react', '@typescript-eslint', 'unused-imports' /* ,"react-hooks" */],
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'unused-imports' /* ,"react-hooks" */
+  ],
   rules: {
     'no-unused-vars': 2,
     'unused-imports/no-unused-imports': 2,
@@ -24,25 +28,6 @@ module.exports = {
     'no-plusplus': 2,
     'no-underscore-dangle': 2,
     'no-console': 2,
-    'no-debugger': 2,
-
-    // "react-hooks/rules-of-hooks": "error", // 检查 Hook 的规则
-
-    // "@typescript-eslint/no-unused-vars": 2,
-    // "@typescript-eslint/no-unused-imports": 2,
-    // "arrow-body-style": 0,
-    // "jsx-a11y/label-has-for": 0,
-    // "max-lines-per-function": [
-    //   2,
-    //   { max: 320, skipComments: true, skipBlankLines: true },
-    // ],
-    // "no-confusing-arrow": 0,
-    // "no-nested-ternary": 0,
-    // "no-console": 2,
-    // "no-param-reassign": [
-    //   2,
-    //   { props: true, ignorePropertyModificationsFor: ["draft"] },
-    // ],
-    // "react/no-this-in-sfc": 0,
-  },
-};
+    'no-debugger': 2
+  }
+}
